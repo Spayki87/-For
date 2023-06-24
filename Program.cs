652736d2825d9a62d9d331819012d0912cf7dce3
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-24 21:13:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Освоение_циклов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+            int userNumber;
+            int repetitionsNumber;
+
+            Console.WriteLine("Введите сообщение, которое хотите повторить:");
+            userMessage = Console.ReadLine();
+            Console.WriteLine("Введите количество повторений сообщения:");
+            userNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (repetitionsNumber = 0; repetitionsNumber < userNumber; repetitionsNumber++)
+            {
+                Console.WriteLine(userMessage);
+            }
+        }
+    }
+}
